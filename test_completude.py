c615a7751b61d003cba037e95b54fcef70ff13c7
samir <samzri@gmail.com>
2023-04-21 11:24:45 +0200

    Docker updated

---
+++ test_completude.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+# Load data from source system
+source_data = pd.read_csv("source_data.csv")
+
+# Load data from target system
+target_data = pd.read_csv("target_data.csv")
+
+# Check if all data from source system is in target system
+assert len(source_data) == len(target_data), "Data migration incomplete"
